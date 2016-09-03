53b269574f4874a12aa854f0cf349f909c1d254f
paul <paul.c.hansen@gmail.com>
2016-09-03 12:22:21 -0700

    put comment in get_Index3D.m

--- Subfunctions/get_Index3D.m
+++ Subfunctions/get_Index3D.m
@@ -1,5 +1,5 @@
 function [id_x1, id_x2, id_x3, id_v1, id_v2, id_v3]= get_Index3D(nParticle)
-
+% [id_x1, id_x2, id_x3, id_v1, id_v2, id_v3]= get_Index3D(nParticle)
 id_x1 = 1:nParticle;
 id_x2 = id_x1 + nParticle;
 id_x3 = id_x2 + nParticle;
